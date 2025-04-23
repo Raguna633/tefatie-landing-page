18c3aa5afed111fefcc2e4f4d51cfe64bf44d729
Ken-ramadhan <ramadhanschool01@gmail.com>
2025-04-23 09:22:05 +0700

    push

--- admin-landing-page/resources/views/partials/dashboard/_app_toast.blade.php
+++ admin-landing-page/resources/views/partials/dashboard/_app_toast.blade.php
@@ -1,28 +1,31 @@
 <script type="text/javascript">
     {{-- Success Message --}}
-    @if (Session::has('success'))
+    @if (session()->has('success'))
     Swal.fire({
-    icon: 'success',
-    title: 'Done',
-    text: '{{ Session::get("success") }}',
-    confirmButtonColor: "#3a57e8"
+        icon: 'success',
+        title: 'Done',
+        text: '{{ session("success") }}',
+        confirmButtonColor: "#3a57e8"
     });
     @endif
-    {{-- Errors Message --}}
-    @if (Session::has('error'))
+
+    {{-- Error Message --}}
+    @if (session()->has('error'))
     Swal.fire({
-    icon: 'error',
-    title: 'Opps!!!',
-    text: '{{Session::get("error")}}',
-    confirmButtonColor: "#3a57e8"
+        icon: 'error',
+        title: 'Opps!!!',
+        text: '{{ session("error") }}',
+        confirmButtonColor: "#3a57e8"
     });
     @endif
-    @if(Session::has('errors') || ( isset($errors) && is_array($errors) && $errors->any()))
+
+    {{-- Validation Errors --}}
+    @if ($errors->any())
     Swal.fire({
-    icon: 'error',
-    title: 'Opps!!!',
-    text: '{{Session::get("errors")->first() }}',
-    confirmButtonColor: "#3a57e8"
+        icon: 'error',
+        title: 'Opps!!!',
+        text: '{{ $errors->first() }}',
+        confirmButtonColor: "#3a57e8"
     });
     @endif
 </script>
